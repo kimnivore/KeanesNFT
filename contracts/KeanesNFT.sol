a5ac73bbfde84dca3aa08be26725bb0c478d2a1d
Kim Nguyen <kimnivore@gmail.com>
2022-04-19 14:28:24 -0700

    updated svg color randomizer

--- contracts/KeanesNFT.sol
+++ contracts/KeanesNFT.sol
@@ -14,11 +14,17 @@ contract KeanesNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    string baseSVG = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: black; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='pink' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    // string baseSVG = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: black; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='pink' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+    string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
 
     string[] firstWords = ["Silly", "Funny", "Fantastic", "Jolly", "Cranky", "Spooky", "Awesome", "Zany", "Kookie", "Terrible", "Epic", "Hilarious", "Amazing", "Magnificent", "Grumpy"];
-    string[] secondWords = ["NoodleSoup", "GarlicNoodles", "IceCream", "Cookies", "FriedChicken", "Fries", "Rice", "Karaage", "Candy", "Cheese", "Baguette", "Pizza", "Popsicles", "Lemonade", "Strawberries"];
+    string[] secondWords = ["Keaney", "Trissy", "Kimmy", "Curtis"];
     string[] thirdWords = ["Kirby", "Daroach", "DarkMetaKnight", "KingDedede", "BandanaWaddleDee", "Bugzzy", "Chirby", "KnuckleJoe", "Vividria", "Magolor", "ChefKawasaki", "Magolor", "PoppyBrosJr", "Plugg", "Chilly"];
+
+    string[] colors = ["#ff0a54", "#ff477e", "#ff5c8a", "#ff7096", "#ff85a1", "#ff99ac", "#fbb1bd", "#f9bec7", "#f7cad0", "#fae0e4"];
 
     event NewEpicNFTMinted(address sender, uint256 tokenId);
 
@@ -36,12 +42,16 @@ contract KeanesNFT is ERC721URIStorage {
         uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
         rand = rand % secondWords.length;
         return secondWords[rand];
-
     }
     function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
         uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
         rand = rand % thirdWords.length;
         return thirdWords[rand];
+    }
+    function pickRandomColor(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+        rand = rand % colors.length;
+        return colors[rand];
     }
 
     function random(string memory input) internal pure returns (uint256) {
@@ -56,7 +66,8 @@ contract KeanesNFT is ERC721URIStorage {
         string memory third = pickRandomThirdWord(newItemId);
         string memory combinedWord = string(abi.encodePacked(first, second, third));
 
-        string memory finalSvg = string(abi.encodePacked(baseSVG, combinedWord, "</text></svg>"));
+        string memory randomColor = pickRandomColor(newItemId);
+        string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
 
         string memory json = Base64.encode(
             bytes(
