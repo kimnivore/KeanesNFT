5bec5ba715b2779f0c0f0f9c4804b0faded8dfb6
Kim Nguyen <kimnivore@gmail.com>
2022-04-20 11:51:13 -0700

    updated env to add etherscan apikey

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config();
 
 
 module.exports = {
   solidity: '0.8.1',
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_APIKEY
+  },
   networks: {
     rinkeby: {
       url: process.env.STAGING_ALCHEMY_KEY,
